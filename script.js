67f819fa8697a7ae225cebbec45733c1c9de53d3
ermesand <dafka100@gmail.com>
2023-03-19 22:37:03 +0300

    popup: добавлен js

--- script.js
+++ script.js
@@ -2,10 +2,43 @@ let edit = document.querySelector('.profile__edit');
 edit.addEventListener('click', function(event) {
     let popup = document.querySelector('.popup');
     popup.classList.add("popup_opened");
+    let nameInput = document.querySelector('.popup__name');
+    let prof_title = document.querySelector('.profile__title');
+    nameInput.setAttribute("value", prof_title.textContent.replace(/[\n\r]+|[\s]{2,}/g, ' ').trim());
+    let jobInput = document.querySelector('.popup__description');
+    let prof_subtitle = document.querySelector('.profile__subtitle');
+    jobInput.setAttribute("value", prof_subtitle.textContent.replace(/[\n\r]+|[\s]{2,}/g, ' ').trim());
+
+    let formElement = document.querySelector('.popup__container');
+    function handleFormSubmit (evt) {
+        evt.preventDefault();
+        prof_title.textContent = nameInput.value;
+        prof_subtitle.textContent = jobInput.value;
+    }
+    formElement.addEventListener('submit', handleFormSubmit);
+
+    let close = document.querySelector('.popup__close');
+    let save = document.querySelector('.popup__save');
+    
+    close.addEventListener('click', function(event) {
+        popup.classList.remove("popup_opened");
+    });
+    save.addEventListener('click', function(event) {
+        popup.classList.remove("popup_opened");
+    });
 });
-let pop_name = document.querySelector('.popup__name');
-let prof_title = document.querySelector('.profile__title');
-pop_name.setAttribute("value", prof_title.textContent.replace(/[\n\r]+|[\s]{2,}/g, ' ').trim());
-let pop__description = document.querySelector('.popup__description');
-let prof_subtitle = document.querySelector('.profile__subtitle');
-pop__description.setAttribute("value", prof_subtitle.textContent.replace(/[\n\r]+|[\s]{2,}/g, ' ').trim());
+
+
+function like(){
+    let like = document.querySelectorAll('.elements__like');
+    for(let i = 0 ; i < like.length; i++ ){
+    like[i].addEventListener('click', function(event) {
+    if (like[i].classList.contains('elements__like_active') === true) {
+        like[i].classList.remove('elements__like_active');
+    } else {
+        like[i].classList.add('elements__like_active');
+    }
+    });
+    }
+}
+like();
