af0d31ddca2deeb954724fb0073cbb336d022378
ermesand <dafka100@gmail.com>
2023-03-20 22:01:12 +0300

    mistakes: исправлено все, кроме js(утечки?)

--- script.js
+++ script.js
@@ -2,28 +2,22 @@ let edit = document.querySelector('.profile__edit');
 edit.addEventListener('click', function(event) {
     let popup = document.querySelector('.popup');
     popup.classList.add("popup_opened");
-    let nameInput = document.querySelector('.popup__name');
-    let prof_title = document.querySelector('.profile__title');
-    nameInput.setAttribute("value", prof_title.textContent.replace(/[\n\r]+|[\s]{2,}/g, ' ').trim());
-    let jobInput = document.querySelector('.popup__description');
-    let prof_subtitle = document.querySelector('.profile__subtitle');
-    jobInput.setAttribute("value", prof_subtitle.textContent.replace(/[\n\r]+|[\s]{2,}/g, ' ').trim());
-
-    let formElement = document.querySelector('.popup__container');
+    let nameInput = document.querySelector('.popup__input_type_name');
+    let jobInput = document.querySelector('.popup__input_type_description');
+    let profTitle = document.querySelector('.profile__title');
+    let profSubtitle = document.querySelector('.profile__subtitle');
+    nameInput.value = profTitle.textContent;
+    jobInput.value = profSubtitle.textContent;
+    let formElement = document.querySelector('.popup__form');
     function handleFormSubmit (evt) {
         evt.preventDefault();
-        prof_title.textContent = nameInput.value;
-        prof_subtitle.textContent = jobInput.value;
+        profTitle.textContent = nameInput.value;
+        profSubtitle.textContent = jobInput.value;
+        popup.classList.remove("popup_opened");
     }
     formElement.addEventListener('submit', handleFormSubmit);
-
     let close = document.querySelector('.popup__close');
-    let save = document.querySelector('.popup__save');
-    
     close.addEventListener('click', function(event) {
-        popup.classList.remove("popup_opened");
-    });
-    save.addEventListener('click', function(event) {
         popup.classList.remove("popup_opened");
     });
 });
