77ec198f8ee6ce30e67a38791e618a5af9e85858
ermesand <dafka100@gmail.com>
2023-03-21 18:24:25 +0300

    correct: переделан  js, центрирован popup, кнопка edit не сжимается

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
-let edit = document.querySelector('.profile__edit');
-edit.addEventListener('click', function(event) {
-    let popup = document.querySelector('.popup');
+let popup = document.querySelector('.popup');
+function open(){
+    let edit = document.querySelector('.profile__edit');
+    edit.addEventListener('click', function(event) {
     popup.classList.add("popup_opened");
+});
+}
+open();
+function close(){
+    let close = document.querySelector('.popup__close');
+    close.addEventListener('click', function(event) {
+        popup.classList.remove("popup_opened");
+    });
+}
+close();
+function save() {
     let nameInput = document.querySelector('.popup__input_type_name');
     let jobInput = document.querySelector('.popup__input_type_description');
     let profTitle = document.querySelector('.profile__title');
     let profSubtitle = document.querySelector('.profile__subtitle');
+    let formElement = document.querySelector('.popup__form');
     nameInput.value = profTitle.textContent;
     jobInput.value = profSubtitle.textContent;
-    let formElement = document.querySelector('.popup__form');
     function handleFormSubmit (evt) {
         evt.preventDefault();
         profTitle.textContent = nameInput.value;
@@ -16,13 +28,9 @@ edit.addEventListener('click', function(event) {
         popup.classList.remove("popup_opened");
     }
     formElement.addEventListener('submit', handleFormSubmit);
-    let close = document.querySelector('.popup__close');
-    close.addEventListener('click', function(event) {
-        popup.classList.remove("popup_opened");
-    });
-});
-
-
+    
+}
+save();
 function like(){
     let like = document.querySelectorAll('.elements__like');
     for(let i = 0 ; i < like.length; i++ ){
